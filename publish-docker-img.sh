ff2d9680b0a7ab20bf236c118056fd01bd5fb650
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-15 21:52:25 +0200

    Update docker publish action

--- publish-docker-img.sh
+++ publish-docker-img.sh
@@ -7,6 +7,8 @@ IFS=$'\n'      # Change IFS to new line
 tags=($tags) # split to array $names
 IFS=$SAVEIFS   # Restore IFS
 
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+
 for (( i=0; i<${#tags[@]}; i++ ))
 do
   echo "Tag name: ggjnez92/vpn2go-frontend:${tags[$i]}"
