a7698446ceda754b979255acaf84cf51a4ea34fa
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-15 02:11:42 +0200

    Add mock logOut

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../core/services';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) {
+  }
 
   ngOnInit(): void {
   }
 
+  async logOut() {
+    await this.auth.logOut();
+  }
 }
