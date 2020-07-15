84bb4a2f9321739008f562a71def0a1b861da8c8
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-15 14:51:41 +0200

    Mock fetchListOfUsers result in vpn service

---
+++ src/app/core/models/list-response.ts
@@ -0,0 +1,4 @@
+export interface ListResponse {
+  titles: string[];
+  items: any[];
+}
--- src/app/core/services/vpn.service.ts
+++ src/app/core/services/vpn.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
+import { CSVDecoder } from '../utils';
+import { ListResponse } from '../models';
+
 @Injectable()
 export class VpnService {
 
   constructor() {
+  }
+
+  fetchListOfUsers(): Observable<ListResponse> {
+    const result = 'name,begin,end,status\n' +
+      'test,Jul 15 11:24:36 2020 GMT,Jul 14 11:24:36 2024 GMT,VALID\n' +
+      'test2,Jul 15 11:24:55 2020 GMT,Jul 14 11:24:55 2024 GMT,VALID\n';
+    const decoder = new CSVDecoder(result);
+    const titles = decoder.nextLine();
+    const items = decoder.decode();
+    return of({titles, items} as ListResponse);
   }
 }
