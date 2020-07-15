aac02b06ea3e8e0260ef718060b03989002e8fed
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-15 22:18:58 +0200

    Improve publish script code

--- publish-docker-img.sh
+++ publish-docker-img.sh
@@ -12,12 +12,15 @@ docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 echo $(git status)
 echo $tags
 
+# build docker image
+docker build -t img .
+
 for (( i=0; i<${#tags[@]}; i++ ))
 do
   echo "Tag name: ggjnez92/vpn2go-frontend:${tags[$i]}"
 
-  # build docker image
-  docker build . -t "ggjnez92/vpn2go-frontend:${tags[$i]}"
+  # tag current image
+  docker tag img "ggjnez92/vpn2go-frontend:${tags[$i]}"
 
   # push docker image to DockerHub
   docker push "ggjnez92/vpn2go-frontend:${tags[$i]}"
