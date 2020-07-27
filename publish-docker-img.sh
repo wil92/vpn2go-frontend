ecc83b1ae4126090882887165558110ba71ebcd0
Guillermo Gonzalez <ggjnez92@gmail.com>
2020-07-27 22:00:58 +0200

    Update publish-docker-img.sh configuration script

--- publish-docker-img.sh
+++ publish-docker-img.sh
@@ -7,33 +7,25 @@ fi
 
 tags=$(git tag --points-at HEAD)
 
-if [ ! -z "$DOCKER_USER" ] && [ ! -z "$DOCKER_PASSWORD" ]
-then
+SAVEIFS=$IFS   # Save current IFS
+IFS=$'\n'      # Change IFS to new line
+tags=($tags) # split to array $names
+IFS=$SAVEIFS   # Restore IFS
 
-  SAVEIFS=$IFS   # Save current IFS
-  IFS=$'\n'      # Change IFS to new line
-  tags=($tags) # split to array $names
-  IFS=$SAVEIFS   # Restore IFS
+echo "$DOCKER_PASSWORD" | docker login -u ${DOCKER_USER} --password-stdin
 
-  echo "$DOCKER_PASSWORD" | docker login -u ${DOCKER_USER} --password-stdin
+# build docker image
+docker build -t img .
 
-  # build docker image
-  docker build -t img .
-
-  for (( i=0; i<${#tags[@]}; i++ ))
-  do
-    echo "---------------------------------"
-    echo "TAG NAME: ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}"
-
-    # tag current image
-    docker tag img ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}
-
-    # push docker image to DockerHub
-    docker push ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}
-    echo "---------------------------------"
-  done
-else
+for (( i=0; i<${#tags[@]}; i++ ))
+do
   echo "---------------------------------"
-  echo "NO PUSH TO DOCKER_HUB"
+  echo "TAG NAME: ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}"
+
+  # tag current image
+  docker tag img ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}
+
+  # push docker image to DockerHub
+  docker push ${DOCKER_USER}/vpn2go-frontend:${tags[$i]}
   echo "---------------------------------"
-fi
+done
